855d4e76791f7c65a97bb7a43857be57500d21d7
Alex Humphreys <alex.humphreys@here.com>
2019-03-04 16:26:33 +0100

    parse block types
    Signed-off-by: Alex Humphreys <alex.humphreys@here.com>

--- generate-dhall.rb
+++ generate-dhall.rb
@@ -7,6 +7,36 @@ resources = JSON.parse(file)
 
 aws_instance = resources.first
 
+# enum NestingMode {
+  # INVALID = 0;
+  # SINGLE = 1;
+  # LIST = 2;
+  # SET = 3;
+  # MAP = 4;
+# }
+
+def print_block_type(name, block_type)
+  type_name = classify(name)
+  if block_type.list
+    type = "List #{type_name}"
+  else
+    type = type_name
+  end
+
+  case
+  when block_type.unknown
+    "Unknown #{type} #{block_type.args}"
+  when block_type.optional && block_type.list
+    "Optional (#{type})"
+  when block_type.optional
+    "Optional #{type}"
+  when block_type.list
+    "List #{type}"
+  else
+    "Unknown #{type} #{block_type.args}"
+  end
+end
+
 def convert_type(attribute)
   case attribute["type"]
   when "Bool"
@@ -33,14 +63,14 @@ def convert_type(attribute)
 end
 
 def print_optional(attribute)
-  if attribute["optional"]
-    "Optional "
-  else
+  if attribute["required"]
     ""
+  else
+    "Optional "
   end
 end
 
-def dhall_field_from_arg(attribute)
+def field_from_arg(attribute)
   # "name": "vpc_security_group_ids",
   # "type": "Set(String)",
   # "required": false,
@@ -89,25 +119,56 @@ in { #{classify(name)} = #{classify(name)} }
   str
 end
 
-def types_from_resource(resource)
-  split = resource['attributes'].group_by { |a| a['optional'] }
-  required = split[false].map { |a| dhall_field_from_arg(a) }
-  optional = split[true].map { |a| dhall_field_from_arg(a) }
+def fields_from_resource(resource)
+  split = resource['attributes'].group_by { |a| a['required'] }
+  required = split[true]&.map { |a| field_from_arg(a) } || []
+  optional = split[false]&.map { |a| field_from_arg(a) } || []
   { optional: optional, required: required }
 end
 
-def dhall_file_from_resource(resource)
-  types = types_from_resource resource
-  # TODO handle empty optional/required
+
+def resource_as_dhall_string(fields)
   file_content =
-    print_type(resource["resource_title"], types[:optional], true) \
-    + print_type(resource["resource_title"], types[:required]) \
-    + print_in_block(resource["resource_title"], types)
+    print_type(resource["type_name"], fields[:optional], true) \
+    + print_type(resource["type_name"], fields[:required]) \
+    + print_in_block(resource["type_name"], fields)
   file_content
 end
 
-content_aws_instance = dhall_file_from_resource aws_instance
+BlockType = Struct.new(:optional, :list, :unknown, :args)
+def block_type(block)
+  args = [block["nesting"], block["min_items"], block["max_items"]]
 
-File.write('./dhall/aws_instance.dhall', content_aws_instance)
+  case args
+  when [2, 0, 1]
+    BlockType.new(true, false, false, args)
+  when [3, 0, 0]
+    BlockType.new(true, true, false, args)
+  else
+    BlockType.new(false, false, true, args)
+  end
+end
+
+DhallRepresentation = Struct.new(:fields, :blocks, :type_name, :block_type)
+def dhall_representation_from_resource(resource)
+  fields = fields_from_resource resource
+  blocks = resource["block_types"].map { |bt| dhall_representation_from_resource(bt) }
+  if resource["nesting"]
+    bt = block_type(resource)
+  else
+    bt = nil
+  end
+
+  DhallRepresentation.new(
+    fields,
+    blocks,
+    resource["type_name"],
+    bt
+  )
+end
+
+dhall_reps_instance = dhall_representation_from_resource aws_instance
+
+#File.write('./dhall/aws_instance.dhall', content_aws_instance)
 
 pry.inspect
--- generate-schema.rb
+++ generate-schema.rb
@@ -6,7 +6,7 @@ resources = resources_string.split("\n")
 resource_args = resources.map do |r|
   print "#{r}\n"
   JSON.parse(`tfschema resource show -format=json #{r}`)
-    .merge({ 'resource_title' => r })
+    .merge({ 'type_name' => r })
 end
 
 File.write("schema/tf-aws-schema.json", JSON.pretty_generate(resource_args))
